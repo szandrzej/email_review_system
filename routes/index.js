734f27e07036a443c63a547c886ca6763b6162b1
Andrzej Szmajnta <andrzej.szmajnta.dev@gmail.com>
2018-04-19 12:08:27 +0200

    Add routes for reviews API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const router = express.Router()
-const { validationResult } = require('express-validator/check')
+const { body, validationResult } = require('express-validator/check')
+const { retrieveReviews } = require('./reviewQueries')
+const { publishReview, pushNewReview, updateReview } = require('./reviewCommands')
 const HttpStatus = require('http-status-codes')
+
+const rulesForCreateReview = [
+  body('text').exists().not().isEmpty(),
+  body('email').exists().isEmail().not().isEmpty()
+]
+
+const rulesForUpdateReview = [
+  body('text').exists().not().isEmpty()
+]
+
+const rulesForPublishReview = [
+  body('publish').exists().isBoolean()
+]
+
+router.get('/reviews', retrieveReviews)
+
+router.post('/reviews', [ rulesForCreateReview, checkErrors ], pushNewReview)
+router.put('/reviews/:reviewId', [ rulesForUpdateReview, checkErrors ], updateReview)
+router.patch('/reviews/:reviewId', [ rulesForPublishReview, checkErrors ], publishReview)
 
 function checkErrors (req, res, next) {
   const errors = validationResult(req)

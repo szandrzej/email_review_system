86a71eef6397a1a92aa6758adbab856141e364d7
andrzej_sz <andrzej.szmajnta.dev@gmail.com>
2018-04-19 16:03:23 +0200

    API: Retrive reviews endoint (#4)
    * Implement fetch logic
    
    * Endpoint tests, check query is boolean

---
+++ commons/schema/Review.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ReviewSchema = new Schema({
+
+  email: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: [ 'likely positive', 'maybe negative' ],
+    required: true
+  },
+  published: {
+    type: Boolean,
+    default: false
+  },
+  created_at: { type: Date, default: Date.now }
+})
+
+
+module.exports = ReviewSchema
